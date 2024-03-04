6bb896b53bf36ea8097e45f253eae9f89d6b9776
CaptainBrexit <3cwwftnf@googlemail.com>
2024-03-05 08:23:15 +1300

    made maeda clock #5, non functional
    made a non functioning version of maeda clock 5, couldnt get custom font working as hoped

--- maeda_clock.js
+++ maeda_clock.js
@@ -3,7 +3,25 @@ function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(50); //  beige
   fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  //pixeloid = loadFont('pixeloid-font/PixeloidSans-mLxMm.tff');
+  //textFont(pixeloid);
+  angleMode(DEGREES);
+  translate(400, 250);
+
+  fill(0);
+  translate(100, -160);
+  textSize(200);
+  rotate(140);
+  text("42", 0, 0);
+
+  translate(160, -10);
+  rotate(-90);
+  fill(255);
+  textSize(120);
+  text("20", 0, 0);
+
+  fill(255, 0, 0);
+  translate(20, 10);
+  textSize(160);
+  text("4", 0, 0);
 }
